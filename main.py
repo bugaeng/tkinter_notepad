3b89e30a78ba4b7afda8b616bb8fb8a8a2a122f5
bugaeng <livekor62@gmail.com>
2023-11-09 09:36:28 +0900

    create menu bar

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ window.title("NotePad")
 window.geometry("400x400")
 window.resizable(False,False)
 
+#메뉴 바 추가
+menu=Menu(window)
+menu_1=Menu(menu,tearoff=0)
+menu_1.add_command(label="New_file")
+menu_1.add_command(label="Save")
+menu_1.add_separator()
+menu_1.add_command(label="Exit",command=window.destroy)
+menu.add_cascade(label="File",menu=menu_1)
+menu_2=Menu(menu,tearoff=0)
+menu_2.add_command(label="Made By Bugaeng")
+menu.add_cascade(label="Maker",menu=menu_2)
+window.config(menu=menu)
+
+window.mainloop()
