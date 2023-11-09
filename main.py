ea1782f9358d8f0e1f34fae13ee91891da8ee169
bugaeng <livekor62@gmail.com>
2023-11-09 09:29:16 +0900

    create tkinter window

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-from
+from tkinter import *
+from tkinter.filedialog import *
+
+#창 크기, 사이징불가, 이름 지정
+window = Tk()
+window.title("NotePad")
+window.geometry("400x400")
+window.resizable(False,False)
+
